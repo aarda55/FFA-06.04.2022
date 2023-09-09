105be1da326c57742f24d966275216c3dca57810
SIEA <69807609+sieadev@users.noreply.github.com>
2023-09-09 17:42:35 +0200

    Update Leave.java

--- src/Listener/Leave.java
+++ src/Listener/Leave.java
@@ -16,16 +16,15 @@ public class Leave implements Listener {
 	
 	@EventHandler
 	public void onInt(PlayerInteractEvent e) {
-	Player p = e.getPlayer();
-	try{
-		if(e.getItem().getType() == Material.INK_SACK){
-		if(e.getItem().getItemMeta().getDisplayName().equalsIgnoreCase("§4Leave")){
-		p.teleport(Data.spawn);
-		Methods.setLobbyItems(p);
-}
-}
-}catch(Exception e1){
-		p.sendMessage("§cThere was an error while teleporting you to the lobby.");
-}
+		Player p = e.getPlayer();
+		try{
+			if(e.getItem().getType() == Material.INK_SACK){
+			if(e.getItem().getItemMeta().getDisplayName().equalsIgnoreCase("§4Leave")){
+			p.teleport(Data.spawn);
+			Methods.setLobbyItems(p);
+		}catch(Exception e1){
+			p.sendMessage("§cThere was an error while teleporting you to the lobby.");
+		}
+		
 	}
 }
